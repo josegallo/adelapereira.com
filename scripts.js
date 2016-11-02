05cefcf199ba1c522c063d6bfcdc8463d0a6f79a
josegallo <josegallo@gmail.com>
2016-11-02 23:22:11 +0100

    Translate strings into Spanish changing texts

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// align ads in blog with optin
+// align ads in blog with optin. Placed on OptimizePress>blogsettings>modules>otherscripts
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 
@@ -24,4 +24,21 @@ $(".widget h4, .widget_categories li").css({ //center texts
 }); 
 
 </script>
+
+
+//change text to Spanish on Leave response section on post blog. Put it on each post
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+
+<script>
+$(document).ready(function(){
+$("#leave-reply h3").text("Deja tu comentario");
+$("#leave-reply label:first-child span:nth-child(2)").text("Nombre*");
+$("#leave-reply label:nth-child(3) span:nth-child(2)").text("Pagina web");
+$("#leave-reply .form-submit input").val("Envía comentario");
+$("#leave-reply .form-submit p").text("* Requerido");
+}); 
+
+	
+</script>
+
 
