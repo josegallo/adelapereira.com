fce6a5579de94e825fa4ef3ec62be0794da5018b
josegallo <josegallo@gmail.com>
2016-11-02 22:33:38 +0100

    Debuging

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 
 <script>
+// align ads in blog with optin
 $(document).ready(function(){
 var optimeWidth = $("div.op_signup_form.op-signup-style-style5").outerWidth();
 var adsBlockWidth = $(".sidebar-ads img:first-child").width();
@@ -12,7 +13,6 @@ console.log (optimeWidth,adsBlockWidth);
 var difference = (optimeWidth - adsBlockWidth)/2;
 
 console.log(difference);
-console.log(numItemSlideBar);
 $(".sidebar-ads a img").css({ 'padding-left': difference + 'px'}); //align to center
 $("div#categories-3").css({ // put the categories 
 		    'margin-left': '0px', 
