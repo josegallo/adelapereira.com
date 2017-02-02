cf79e621d40106a57df2f4d8ad22fa1ce5b00fe7
josegallo <josegallo@gmail.com>
2017-02-02 23:54:05 +0100

    color copyright at footer

--- scripts-samcart.js
+++ scripts-samcart.js
@@ -56,5 +56,14 @@ $(document).ready(function(){
 	});
 </script>
 
+//color copyright at footer
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+	$("footer.footer .footer-decoration .middle-container div p").css({ 'color': '#f8f8f8'});
+	});
+</script>
+
 
 	
